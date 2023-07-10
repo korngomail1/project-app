44373df76d4f82037fa83bd8192b3dcb86a2d681
korngomail1 <korn_592max@hotmail.com>
2023-07-10 22:32:38 +0700

    scc

--- resources/views/admin/schedule/index.blade.php
+++ resources/views/admin/schedule/index.blade.php
@@ -1,2 +1,11 @@
 <x-app-layout>
+    <table class="table table-sm table-striped table-bordered table-hover">
+        <thead>
+            <tr class="info thead-bg">
+                <th class="text-center">ID</th>
+                <th class="text-center">ชื่อ / สกุล</th>
+                <th class="text-center">manage</th>
+            </tr>
+        </thead>
+    </table>
 </x-app-layout>
